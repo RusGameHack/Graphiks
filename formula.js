a57146db687b3ea2b78c6c3f76918e59241a05f5
Artur Safin <rusgamehack@gmail.com>
2023-04-07 17:50:43 +0300

    FixForm

---
+++ formula.js
@@ -0,0 +1,33 @@
+let primeri = document.querySelectorAll('.blockOutput'),
+    znInput = document.querySelector('.znIn'),
+    chInput = document.querySelector('.chIn'),
+    activeIn = '';
+
+//Выбор поля для формул
+znInput.addEventListener('click', (e)=>{
+    clickPole(e.target);
+});
+chInput.addEventListener('click', (e)=>{
+    clickPole(e.target);
+});
+const clickPole = (block) =>{
+    block.classList.add('active');
+    if(block.classList.contains('znIn')){chInput.classList.remove('active');} else {znInput.classList.remove('active');}
+    activeIn = block;
+}
+
+
+//Ввод формул в поле
+primeri.forEach(primer => {
+    primer.addEventListener('click', ()=>{
+        console.log('click')
+        if(activeIn==""){
+            document.querySelector('.error').innerHTML = 'Пожалуста, выберите числитель, либо знаменатель';
+            return 0;
+        }
+        else {
+            document.querySelector('.error').innerHTML = '';
+            activeIn.innerHTML += `<div class="${primer.id}">(${primer.innerHTML})</div>`;
+        }
+    });
+});
